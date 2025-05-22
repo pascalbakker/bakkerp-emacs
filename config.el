59c14710895a0be980196f6e46063442c5c30d9f
pascalbakker <pascalsbakker@gmail.com>
2025-05-21 23:35:11 -0500

    feat: Added keybindings to allow workspace switching in vterm

--- config.el
+++ config.el
@@ -306,3 +306,15 @@
   (setq-local line-spacing 0.2))
 
 (add-hook 'nov-mode-hook 'my/nov-reader-mode)
+
+;; Vterm mapping
+(after! vterm
+  (evil-define-key 'normal vterm-mode-map
+    (kbd "M-{") #'+workspace/switch-left
+    (kbd "M-}") #'+workspace/switch-right))
+
+;; Sets (kbd "M-[0-9]") +workspace/switch-to-[0-9]
+(dotimes (i 10)
+  (let ((key (format "M-%d" (1+ i))) ; M-1 to M-9 and M-10 (which is M-0)
+        (fn  (intern (format "+workspace/switch-to-%d" i))))
+    (evil-define-key 'normal vterm-mode-map (kbd key) fn)))
