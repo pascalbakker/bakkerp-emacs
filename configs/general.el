f0cf30eb4424b9581055000562b274ab497750a9
Pascal Bakker <pascalsbakker@gmail.com>
2025-10-18 15:27:34 -0500

    feat: Updated GC thresholds for statup

--- configs/general.el
+++ configs/general.el
@@ -21,8 +21,17 @@
 ;; abbr
 (setq-default abbrev-mode t)
 
-;; Garbage collection threshold
-(setq gc-cons-threshold (* 100 1024 1024))
+;;; Improve performance by tweaking garbage collection thresholds
+
+;; Raise threshold during startup (default is ~800KB)
+(setq gc-cons-threshold (* 200 1024 1024))  ;; 200 MB
+(setq gc-cons-percentage 0.6)               ;; Allow GC to use more heap space
+
+;; Reset after startup to a more reasonable value
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (setq gc-cons-threshold (* 100 1024 1024))  ;; 50 MB
+            (setq gc-cons-percentage 0.1)))
 
 ;; Consult live preview
 (setq consult-preview-enable t)
